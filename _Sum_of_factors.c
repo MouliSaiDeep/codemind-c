adb477e9da67bb45ba67eb4ff290f6c95bb374cf
MouliSaiDeep <147487696+MouliSaiDeep@users.noreply.github.com>
2023-11-22 14:42:47 +0530

    #include<stdio.h> int main() {     int a,b,c=0,i;     scanf("%d",&a);     for(i=1;i<a;i++)     {         b=a%i;         if(b==0)         {             c=c+i;         }     }     printf("%d",c); }

---
+++ _Sum_of_factors.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c=0,i;
+    scanf("%d",&a);
+    for(i=1;i<a;i++)
+    {
+        b=a%i;
+        if(b==0)
+        {
+            c=c+i;
+        }
+    }
+    printf("%d",c);
+}
