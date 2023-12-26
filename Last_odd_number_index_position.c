f15bacba8aaae806a68e8970f39ae03aa0913012
MouliSaiDeep <147487696+MouliSaiDeep@users.noreply.github.com>
2023-12-26 23:10:11 +0530

    #include<stdio.h> int main() {     int n,i,arr[n],pos;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==1)         {             pos=i;         }
        }
        printf("%d ",pos);
    }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[n],pos;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==1)
+        {
+            pos=i;
+        }
+        
+    }
+    printf("%d ",pos);
+}
